b96649118d86ef2863ed1166a3358799d2508f32
Shaurya Varshnay <shauryarchs@gmail.com>
2026-05-08 17:36:59 -0700

    deleted old code

--- arduino-source/main/buttons.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-namespace Buttons {
-  enum Event { EVENT_NONE, EVENT_LEFT, EVENT_RIGHT, EVENT_STOP };
-
-  void begin();
-  Event pollEvent();   // call from tick(); returns one press-edge event per call
-}
